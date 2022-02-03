7d00089f9f4c31653d1bcc553684195c2ebd1419
raoniii <raonisferreira@gmail.com>
2022-02-03 18:08:08 -0300

    widget and card

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,58 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
   home: Scaffold(
-    appBar: AppBar(title: Text('Tranfer New'),
-
-),
-
-  floatingActionButton: FloatingActionButton(
-    onPressed: () {
-      // Add your onPressed code here!
-    },
-    backgroundColor: Colors.blueGrey,
-    child: const Icon(Icons.add),
+    body: ListaTransferencias(),
+    appBar: AppBar(
+      title: Text('New transfer'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {  },
+      backgroundColor: Colors.blueGrey,
+      child: Icon(Icons.add),
+    ),
   ),
-)
-)
-);
+));
+
+class ListaTransferencias extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: <Widget>[
+        ItemTransferencias(Transferencia(999.0, 1000)),
+        ItemTransferencias(Transferencia(111.1, 100)),
+
+
+
+
+      ],
+    );
+  }
+}
+
+class ItemTransferencias extends StatelessWidget{
+
+ final Transferencia _transferencia;
+
+ ItemTransferencias(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.conta.toString()),
+      ),
+    );
+  }
+
+}
+
+class Transferencia{
+  final double valor;
+  final double conta;
+
+  Transferencia(this.valor, this.conta);
+}
